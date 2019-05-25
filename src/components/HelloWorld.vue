dc9854b951cbe127c06a56d0bda4d3a858dec593
qiaoliyang <liyang.qiao@hand-china.com>
2019-05-25 14:07:49 +0800

    create

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+    <input type="text" v-model="inputValue" @keyup="addLiat($event)">
+    <br>
+    <ul>
+      <!-- <li v-for="(item,index) in list" :key="index">{{item}}</li> -->
+      <todoItem
+        v-for="(item,index) in list"
+        :key="index"
+        :content="item"
+        :index="index"
+        @delete="deleteItem"
+      ></todoItem>
+    </ul>
+  </div>
+</template>
+
+<script>
+import todoItem from "./todoItem";
+export default {
+  name: "HelloWorld",
+  components: {
+    todoItem
+  },
+  data() {
+    return {
+      inputValue: "",
+      list: []
+    };
+  },
+  methods: {
+    addLiat(e) {
+      if (e.keyCode == 13) {
+        this.list.push(this.inputValue);
+        this.inputValue = "";
+      }
+    },
+    deleteItem(index) {
+      console.log(index);
+      this.list.splice(index, 1);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/todoItem.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+    <div @click="deleteItem">{{content}}</div>
+</div>
+</template>
+<script>
+export default {
+    props:['content','index'],
+    methods:{
+        deleteItem(){
+            this.$emit('delete',this.index)
+        }
+    }
+};
+</script>
+<style>
+</style>
+
+
